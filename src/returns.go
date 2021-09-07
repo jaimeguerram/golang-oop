ff5e7cb2d68e488135549ef8f2080cc3bbe52734
jfguerra <jfguerra@userena.cl>
2021-09-06 21:15:33 -0300

    ffunción variatica

---
+++ src/returns.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+//función variatica
+func sum(values ...int) int {
+	total := 0
+
+	for _, num := range values {
+		total += num
+	}
+
+	return total
+}
+
+//retornos con nombres
+func getValues(x int) (double int, triple int, quad int) {
+	double = 2 * x
+	triple = 3 * x
+	quad = 4 * x
+	return
+}
+
+func main() {
+	fmt.Println(sum(1, 2, 3, 4))
+	fmt.Println(getValues(2))
+}
